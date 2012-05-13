8d27353f195032a042273b87ed0c2a2d16eb4b9d
Tatsuya Kawano <tatsuya6502@gmail.com>
2012-05-13 10:01:52 +0900

    Add some console logs.

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ var web = require("./lib/web/server"),
     syslog = require("./lib/syslog/server")
 
 // Use the supplied app-server port
-web.listen(process.env.PORT || 8001)
+web.listen(process.env.PORT || 8001, function(){
+  console.log("Web server started. Listening at %s:%d in %s mode", 
+              web.address().address,
+              web.address().port,
+              web.settings.env);
+})
 
 // Bind to a really high port so sudo is not needed
 syslog.bind(10514)
